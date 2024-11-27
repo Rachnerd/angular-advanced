90256a0d4825e46173c7aef4fd77e816fc2356dd
Rachèl Heimbach <rachel@openvalue.nl>
2024-11-27 08:04:44 +0100

    Checkbox starter code

---
+++ libs/ui-components/src/checkbox/checkbox.component.stories.ts
@@ -0,0 +1,13 @@
+import type { Meta, StoryObj } from '@storybook/angular';
+import { CheckboxComponent } from './checkbox.component';
+
+const meta: Meta<CheckboxComponent> = {
+  component: CheckboxComponent,
+  title: 'CheckboxComponent',
+};
+export default meta;
+type Story = StoryObj<CheckboxComponent>;
+
+export const Primary: Story = {
+  args: {},
+};
